a4d73fe46978a684641e2469cdffd5acb66dd6ea
ddannyc <weilong712@gmail.com>
2021-01-19 14:01:20 +0800

    test: links and home controller test

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -4,5 +4,24 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get home_index_url
     assert_response :success
+    assert_match 'Home#index', @response.body
+  end
+
+  test 'should link redirect success' do
+    url = Base64.urlsafe_encode64('https://gist.github.com/cfanpnk/3e29ff807be44558acda06f6bce763a1')
+    code = SecureRandom.alphanumeric(7).downcase
+    link = Link.create(url_code: code, origin_url: url, expire_at: Time.now + 5.minutes)
+    view_count = link.view_count
+    get code_path(link.url_code)
+    assert_response :found
+    assert_equal view_count + 1, link.reload.view_count
+  end
+
+  test 'should expires link render not found' do
+    link = links(:one)
+    view_count = link.view_count
+    get code_path(link.url_code)
+    assert_response :not_found
+    assert_not_equal view_count + 1, link.reload.view_count
   end
 end
--- test/controllers/links_controller_test.rb
+++ test/controllers/links_controller_test.rb
@@ -1,11 +1,17 @@
 require "test_helper"
 
 class LinksControllerTest < ActionDispatch::IntegrationTest
+
   test 'should create shortener link' do
     assert_difference('Link.count') do
-
       post links_path,
            params: { url: 'https://gist.github.com/cfanpnk/3e29ff807be44558acda06f6bce763a1' }
     end
   end
+
+  test 'should show link' do
+      get link_path(links(:one))
+      assert_response :success
+  end
+
 end
